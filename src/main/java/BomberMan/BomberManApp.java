37a3f7373b26970a39110e37f7c329bd8d5b5189
alexandre JACOB <143850309+alexandreJACOB9@users.noreply.github.com>
2025-06-06 01:07:45 +0200

    Ajout du menu de démarage

--- src/main/java/BomberMan/BomberManApp.java
+++ src/main/java/BomberMan/BomberManApp.java
@@ -7,18 +7,29 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class BomberManApp extends Application {
-    
+
     @Override
     public void start(Stage primaryStage) throws Exception {
+        // Afficher le menu au démarrage
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/example/BomberMan/MenuView.fxml"));
+        Parent root = loader.load();
+        Scene scene = new Scene(root, 600, 600);
+        primaryStage.setTitle("Super Bomberman");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    // Cette méthode pourra être appelée depuis MenuController pour lancer le jeu
+    public void launchGame(Stage stage) throws Exception {
         FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/example/BomberMan/GameView.fxml"));
         Parent root = loader.load();
         Scene scene = new Scene(root);
         scene.getStylesheets().add(getClass().getResource("/com/example/BomberMan/game.css").toExternalForm());
-        primaryStage.setTitle("BomberMan - JavaFX");
-        primaryStage.setScene(scene);
-        primaryStage.show();
+        stage.setTitle("BomberMan - JavaFX");
+        stage.setScene(scene);
+        stage.show();
     }
-    
+
     public static void main(String[] args) {
         launch(args);
     }
