0e01988ac125b77e76084d4ec689fa48e23a6ce7
alexandre JACOB <143850309+alexandreJACOB9@users.noreply.github.com>
2025-06-06 15:28:24 +0200

    Update MenuController.java

--- src/main/java/BomberMan/MenuController.java
+++ src/main/java/BomberMan/MenuController.java
@@ -3,10 +3,14 @@ package BomberMan;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.stage.Stage;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 
 public class MenuController {
     @FXML private Button playButton;
     @FXML private Button quitButton;
+    @FXML private Button customButton;
     @FXML private Button battleButton;
 
     @FXML
@@ -14,6 +18,7 @@ public class MenuController {
         playButton.setOnAction(event -> launchGame(false));
         battleButton.setOnAction(event -> launchGame(true));
         quitButton.setOnAction(event -> ((Stage) quitButton.getScene().getWindow()).close());
+        customButton.setOnAction(event -> openCustomisationView());
     }
 
     private void launchGame(boolean vsIA) {
@@ -25,6 +30,20 @@ public class MenuController {
             } else {
                 app.launchGame(menuStage);
             }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    private void openCustomisationView() {
+        changeScene("/com/example/BomberMan/FXML/PersonnalisationView.fxml", "Personnalisation", 600, 600, customButton);
+    }
+    private void changeScene(String fxmlPath, String title, int width, int height, Button sourceButton) {
+        try {
+            Stage stage = (Stage) sourceButton.getScene().getWindow();
+            Parent root = FXMLLoader.load(getClass().getResource(fxmlPath));
+            Scene scene = new Scene(root, width, height);
+            stage.setScene(scene);
+            stage.setTitle(title);
         } catch (Exception e) {
             e.printStackTrace();
         }
